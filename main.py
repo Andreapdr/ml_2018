5176ea7642f6f80ba9ae8a5f96d359e4411ddcda
andrea <andrea.pdr@hotmail.it>
2018-10-22 04:24:29 +0200

    fixed backpropagation alg x3

--- main.py
+++ main.py
@@ -15,17 +15,17 @@ if __name__ == "__main__":
     # Watch out: length of dataset and test_data is computed DIFFERENTLY
     # print(len(dataset))
     # print(len(test_data))
-    print(dataset.shape[0])
-    print(test_data.shape[0])
+    # print(dataset.shape[0])
+    # print(test_data.shape[0])
+
+    ''' NB: every layer must have as many weights as the previous layer's neuron
+        SET NETWORK STRUCTURE WITH APPROPRIATE WEIGHT AMOUNTS AND LAYERS
+        NB: SET ON PREPROCESSING MONK DATASET in TRAINING FUNCTION '''
 
     # nn = NeuralNet()            # initialize empty network = list containing layers
     # nn.initialize_layer(3, 6)   # set a first in layer (3 neuron, 6 weights each)
     # nn.initialize_layer(3, 3)   # set hidden layer (3 neuron, 3 weights each)
     # nn.initialize_layer(1, 3)   # set out_layer (1 neuron, 3 weights each)
-                                # NB: every layer must have as many weights as the previous layer's neuron
-                                # NB: SET NETWORK STRUCTURE WITH APPROPRIATE WEIGHT AMOUNTS AND LAYERS
-                                # NB: SET ON PREPROCESSING MONK DATASET in TRAINING FUNCTION
-
     # nn.training(10, dataset, learning_rate=0.002, verbose=False, monksDataset=True)
 
     # Test Purpose network
@@ -34,9 +34,6 @@ if __name__ == "__main__":
     nn.initialize_layer(3, 3)
     nn.initialize_layer(1, 3)
     nn.training(10, test_data, learning_rate=0.5, monksDataset=False)
-
-
-
 
 # TODO: implement a way to assess error in training
 # TODO: INCLUDE BIAS IN ALGORITHM/WHOLE CODE
