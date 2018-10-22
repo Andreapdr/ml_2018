fixed backpropagation alg

layer.py
@@ -6,7 +6,7 @@ class Layer:
 
     def __init__(self, n_neurons, n_neurons_weights):
         self.neurons = list()
-        self.out_vector = np.empty([1, n_neurons])
+        self.out_vector = np.ones([1, n_neurons])
         for i in range(n_neurons):
             self.neurons.append(Neuron(n_neurons_weights))
 

main.py
@@ -10,19 +10,34 @@ def preprocess_dataset(name_csv):
 if __name__ == "__main__":
     name_csv = "monk_dataset.csv"
 
-    dataset= preprocess_dataset(name_csv)
+    dataset = preprocess_dataset(name_csv)
+    test_data = np.array([[1, 2, 3, 10]])
+    # Watch out: length of dataset and test_data is computed DIFFERENTLY
+    # print(len(dataset))
+    # print(len(test_data))
+    print(dataset.shape[0])
+    print(test_data.shape[0])
 
-    nn = NeuralNet()  # initialize empty network = list containing layers
-    nn.initialize_layer(6, 6)  # set a first in layer (2 neuron, 2 weights each)
-    nn.initialize_layer(3, 6)  # set hidden layer (3 neuron, 2 weights each)
-    nn.initialize_layer(1, 3)  # set out_layer (1 neuron, 3 weights each)
+    nn = NeuralNet()            # initialize empty network = list containing layers
+    nn.initialize_layer(3, 6)   # set a first in layer (3 neuron, 6 weights each)
+    nn.initialize_layer(3, 3)   # set hidden layer (3 neuron, 3 weights each)
+    nn.initialize_layer(1, 3)   # set out_layer (1 neuron, 3 weights each)
+                                # NB: every layer must have as many weights as the previous layer's neuron
+                                # NB: SET NETWORK STRUCTURE WITH APPROPRIATE WEIGHT AMOUNTS AND LAYERS
+                                # NB: SET ON PREPROCESSING MONK DATASET in TRAINING FUNCTION
+
+    # Test Purpose network
+    # nn = NeuralNet()
+    # nn.initialize_layer(3, 3)
+    # nn.initialize_layer(3, 3)
+    # nn.initialize_layer(1, 3)
 
     # print("Weights as initialized")
     # for layer in nn.layer_list:
     #     for neuron in layer.neurons:
     #         print(neuron.weights)
 
-    nn.training(1, dataset, verbose=False)
+    nn.training(10, dataset, learning_rate=0.002, verbose=False, monksDataset=True)
 
     # print("\nWeights after first update")
     # for layer in nn.layer_list:
@@ -30,5 +45,5 @@ if __name__ == "__main__":
     #         print(neuron.weights)
 
 # TODO: implement a way to assess error in training
-
+# TODO: INCLUDE BIAS IN ALGORITHM/WHOLE CODE
 

neuron.py
@@ -5,17 +5,20 @@ import math
 class Neuron:
 
     def __init__(self, n_weights):
-        self.weights = np.random.rand(1, n_weights)
+        # self.weights = np.random.rand(1, n_weights)
+        self.weights = np.ones((1, n_weights))
+        self.inputs_list = np.ones((1, n_weights))
         self.network_in = 0.00
         self.output = 0.00
         self.delta = 0.00
 
     def compute_network_in(self, row_inputs):
+        # print(f"Input: {row_inputs} \ntype: {type(row_inputs)}")
+        for i in range(len(row_inputs)):
+            input_x = row_inputs[i]
+            self.inputs_list[0, i] = input_x
         self.network_in = np.dot(row_inputs, self.weights.T)                    # return an ndarray object !
         return self.network_in
-
-    def get_weight(self):
-        return self.weights()
 
     def compute_output(self):
         self.output = sigmoid_function(self.network_in)
@@ -26,14 +29,12 @@ class Neuron:
         self.delta = (target - self.output) * derivative_activation_function
         return self.delta
 
-    def compute_delta_hidden(self, next_layer, index_of_nueron_prev_layer):
+    def compute_delta_hidden(self, next_layer, index_of_nueron_prev_layer):       # TODO: check if derivative is correct (pay attention to: self.output could be the wrong value)
         derivative_activation_function = self.output * (1 - self.output)          # for sigmoidal activation function
         hidden_error = 0.00
         for neuron in next_layer.neurons:                                                   # hidden_error = Delta next_layer neuron
             hidden_error += neuron.delta * neuron.weights[0, index_of_nueron_prev_layer]    # multplied by weight[j] next_layer neuron
-        # print(f"For Neuron: hidden error: {hidden_error}")
         delta_hidden = hidden_error * derivative_activation_function
-        # print(delta_hidden)
         self.delta = delta_hidden
         return delta_hidden
 