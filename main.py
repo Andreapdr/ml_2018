Fixed delta computation; Implemented learning rate decay and momentum.

main.py
@@ -4,16 +4,19 @@ import matplotlib.pyplot as plt
 
 
 def preprocess_dataset(name_csv):
+    # TODO: TO BETTER IMPLEMENT
     dataset_np = np.genfromtxt(name_csv, delimiter=",")
     return dataset_np
 
 
 if __name__ == "__main__":
-    name_csv = "monk_dataset.csv"
 
-    dataset = preprocess_dataset(name_csv)
-    np.random.shuffle(dataset)                  # Shuffle dataset to prevent stagnation on input patterns
-    test_data = np.array([[1, 2, 3, 10]])       # Watch out: length of dataset and test_data is computed DIFFERENTLY
+    train_csv = "monk_dataset.csv"
+    test_csv = "monk_dataset_test.csv"
+
+    training_set = preprocess_dataset(train_csv)
+    test_set = preprocess_dataset(test_csv)
+    # test_data = np.array([[1, 2, 3, 10]])       # Watch out: length of dataset and test_data is computed DIFFERENTLY
 
     ''' NB: every layer must have as many weights as the previous layer's neuron
         SET NETWORK STRUCTURE WITH APPROPRIATE WEIGHT AMOUNTS AND LAYERS
@@ -21,11 +24,16 @@ if __name__ == "__main__":
 
     nn = NeuralNet()            # initialize empty network = list containing layers
     nn.initialize_layer(3, 6)   # set a first in layer (3 neuron, 6 weights each)
-    nn.initialize_layer(3, 3)   # set hidden layer (3 neuron, 3 weights each)
     nn.initialize_layer(1, 3)   # set out_layer (1 neuron, 3 weights each)
-    nn.training(350, dataset, learning_rate=0.002, verbose=False, monksDataset=True)
+
+    # TRAINING SESSION
+    nn.training(450, training_set, learning_rate=0.10, momentum=0.01, verbose=False, monksDataset=True)
+
+    # TEST SESSION
+    nn.test(test_set, monksDataset=True)
 
     # TEST PLOTTING
+    # TODO: TO BETTER IMPLEMENT
     cord_x = list()
     cord_y = list()
     for elem in nn.error_list:
@@ -35,16 +43,4 @@ if __name__ == "__main__":
     plt.xlabel("Epochs number")
     plt.ylabel("Error")
     plt.show()
-
-    # Test Purpose network
-    # nn = NeuralNet()
-    # nn.initialize_layer(150, 6)
-    # nn.initialize_layer(150, 150)
-    # nn.initialize_layer(1, 150)
-
-    # nn.training(1000, test_data, learning_rate=0.5, monksDataset=False)
-    # nn.training(1000, dataset, learning_rate=0.5, verbose=False, monksDataset=True)
-# TODO: implement a way to assess error in training
-# TODO: INCLUDE BIAS IN ALGORITHM/WHOLE CODE
-# TODO: implement momentum
 

neuralNetwork.py
@@ -1,3 +1,4 @@
+import numpy as np
 from layer import Layer
 
 
@@ -14,16 +15,18 @@ class NeuralNet:
     def feedforward(self, row_input):
         actual_input = row_input
         for i in range(len(self.layer_list)):
-            # TODO: TESTING - TO CLEAN UP
-            # if i == len(self.layer_list)-1:
-                # Need to convert last input to 1 if >0.5
-                # layer = self.layer_list[i]
-                # layer.compute_squash_layer_final()
-                # return
             layer = self.layer_list[i]
             layer.compute_input_layer(actual_input)
+            if i == len(self.layer_list)-1:
+                layer.compute_squash_layer()
+                return
             next_input = layer.compute_squash_layer()[0]
             actual_input = next_input
+
+    def compute_output(self, nn_input):
+        for i in range(len(self.layer_list)):
+            layer = self.layer_list[i]
+            layer.compute_input_layer(nn_input)
 
     # Not entirely sure about this error calculation
     def compute_delta_output_layer(self, target):
@@ -50,17 +53,26 @@ class NeuralNet:
 
     # Updating weights: w_new += delta_Wji
     # where delta_Wji = lr * delta_j * input_ji
-    def update_weights(self, learning_rate):
+    def update_weights(self, learning_rate, momentum):
+        previous_weight_update = 0.0
         for layer in self.layer_list:
             for neuron in layer.neurons:
                 for i in range(len(neuron.weights[0])):
-                    new_weight = neuron.weights[0, i] + learning_rate * neuron.delta * neuron.inputs_list[0, i]
+                    momentum_term = momentum * previous_weight_update
+                    weight_update = momentum_term + learning_rate * neuron.delta * neuron.inputs_list[0, i]
+                    previous_weight_update = weight_update
+                    new_weight = neuron.weights[0, i] + weight_update
                     neuron.weights[0, i] = new_weight
 
-    def training(self, n_epochs, tr_set, learning_rate=0.001, verbose=False, monksDataset=False):
+    def training(self, n_epochs, tr_set, learning_rate=0.01, momentum=0.05, verbose=False, monksDataset=False):
+        current_learning_rate =learning_rate
         for j in range(n_epochs):
             print("\nEPOCH {} ___________________________".format(j + 1))
             epoch_error = 0.00
+            # use different order of patterns in different epochs
+            np.random.shuffle(tr_set)
+            # learning rate decreases through epochs
+            current_learning_rate -= 0.001
             for i in range(len(tr_set)):
                 # if verbose:
                 #     print(f"Training_sample {i+1} of {len(tr_set)}")
@@ -70,11 +82,32 @@ class NeuralNet:
                     tr_in, target = self.preprocess_monk_dataset(tr_set[i])
                 self.feedforward(tr_in)
                 err_out = self.compute_delta_output_layer(target)
-                err_hid = self.compute_delta_hidden_layer()
+                self.compute_delta_hidden_layer()
                 epoch_error += err_out
-                self.update_weights(learning_rate)
+                self.update_weights(learning_rate, momentum)
             print(f"Total Error for Epoch: {round(epoch_error, 5)}")
             self.error_list.append((j, epoch_error))
+        print(f"Final NN: Weights:")
+        for layer in self.layer_list:
+            for neuron in layer.neurons:
+                print(neuron.weights)
+
+    def test(self, test_set, monksDataset=False):
+        print("\nTest results:______________________")
+        correct_predictions = 0
+        for i in range(len(test_set)):
+            test_in = test_set[i][:-1]
+            target = test_set[i][-1]
+            if monksDataset:
+                test_in, target = self.preprocess_monk_dataset(test_set[i])
+            self.feedforward(test_in)
+            nn_output = self.layer_list[1].neurons[0].compute_output_final()
+            correct_predictions += 1 - abs(target - nn_output)
+        print("Accuracy:")
+        print(correct_predictions/len(test_set))
+
+
+
 
     # for MonkDataset -> remove last column and set
     # features and first column as target desired output

neuron.py
@@ -5,21 +5,18 @@ import math
 class Neuron:
 
     def __init__(self, n_weights):
-        self.weights = np.random.uniform(low=-1.00, high=1.00, size=(1, n_weights))
+        self.weights = np.random.uniform(low=-0.50, high=0.50, size=(1, n_weights))
         self.bias = 1.00
-        # self.weights = np.ones((1, n_weights))
         self.inputs_list = np.ones((1, n_weights))
         self.network_in = 0.00
         self.output = 0.00
         self.delta = 0.00
 
     def compute_network_in(self, row_inputs):
-        # print(f"Input: {row_inputs} \ntype: {type(row_inputs)}")
         for i in range(len(row_inputs)):
             input_x = row_inputs[i]
             self.inputs_list[0, i] = input_x
         self.network_in = np.dot(row_inputs, self.weights.T)
-        # TODO: SHOULD baias used here also? or after squashing function?
         self.network_in = self.network_in + self.bias
         return self.network_in
 
@@ -28,22 +25,18 @@ class Neuron:
         return self.output
 
     def compute_output_final(self):
-        out = sigmoid_function(self.network_in)
+        out = self.output
         if out > 0.5:
-            self.output = 0.5
+            return 1
         else:
-            self.output = 0.0
+            return 0
 
     # for sigmoidal activation function
     def compute_delta_output(self, target):
-        # TODO: CHECK CODE HERE
-        # print(f"error at compute_delta_output: {target - self.output}")
-        derivative_activation_function = -self.output * (1 - self.output)
-        # print(f"Output: {round(self.output, 4)} Target: {target}")
+        derivative_activation_function = self.output * (1 - self.output)
         self.delta = (target - self.output) * derivative_activation_function
         return self.delta
 
-    # TODO: check if derivative is correct (pay attention to: self.output could be the wrong value)
     def compute_delta_hidden(self, next_layer, index_of_neuron_prev_layer):
         derivative_activation_function = self.output * (1 - self.output)
         hidden_error = 0.00